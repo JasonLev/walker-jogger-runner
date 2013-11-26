3184be56352b60805a8799ce4802abf6fc4a9ec1
JasonLev <jasond.levy@gmail.com>
2013-11-25 16:26:39 -0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalkerJoggerRunner::Application.config.secret_token = '3ee2aed0b704d80f58ee377580a4db87b118f82a01a7559211d330cad4a7cdc1594bab6ade60cca487723b22b10188d0b19f4ccf00241df4f689a6c58e0e98cf'
